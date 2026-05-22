7eaa7363f549c37b5e53f201cad92ef0602c3c92
Harrish M <harrishmuthu66@gmail.com>
2026-05-22 21:19:51 +0530

    Added compound interest calculator

--- JavaFiles/src/Main.java
+++ JavaFiles/src/Main.java
@@ -4,6 +4,6 @@ public class Main
     {
         System.out.print("Hello world");
         System.out.println("\nAddition = " + Calculator.add(10,15));
-        System.out.println("\nInterest = " + InterestCalculator.calculate(100000,8,5));
+        System.out.println("\nCompound Interest = " + CompoundInterestCalculator.calculate(100000,8,5));
     }
 }
